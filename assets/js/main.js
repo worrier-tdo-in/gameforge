17c95a162f4495de8cbf542babad1669927cc512
worrier-tdo <hostaman199@gmail.com>
2025-09-08 18:16:27 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,13 +94,15 @@ if(form){
 
   function closeGate(){ bd.style.display='none'; bd.remove(); }  
   // ✅ Redirect when "Yes" is clicked
-  bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "https://p8r9.com/?utm_campaign=ttN5oc1jmM&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
-  });
+  bd.querySelector('#age-yes').addEventListener('click', closeGate);
+  //                                               function(){
+  //   window.location.href = "https://p8r9.com/?utm_campaign=ttN5oc1jmM&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+  // });
 
   // ✅ Just close modal when "No" is clicked
-  bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "https://p8r9.com/?utm_campaign=ttN5oc1jmM&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
-  });
+  bd.querySelector('#age-no').addEventListener('click', closeGate);
+  //                                              function(){
+  //   window.location.href = "https://p8r9.com/?utm_campaign=ttN5oc1jmM&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+  // });
 })();
 
